0ad3f4b7e3359001e2a184bc2fe5ea158bf82e96
Ogulcan <balogulcanbal@gmail.com>
2017-10-05 11:35:13 +0200

    First commit of all files

---
+++ examples/cc2650stk/sensortag/ccs/config/ccs_linker_defines.cmd
@@ -0,0 +1,10 @@
+/*
+** Stack Frontier Generator 1.1.0 (2017-09-28 19:20:24.286000)
+**
+** WARNING - Auto-generated file. Modifications could be lost!
+*/
+
+--define=ICALL_RAM0_START=0x20004428
+--define=ICALL_STACK0_START=0x11000
+--define=ICALL_STACK0_ADDR=0x11001
+
---
+++ examples/extra_2_02/examples/cc2650lp/simple_central_audio_receiver/ccs/config/ccs_linker_defines.cmd
@@ -0,0 +1,10 @@
+/*
+** Stack Frontier Generator 1.1.0 (2017-03-30 11:36:30.981000)
+**
+** WARNING - Auto-generated file. Modifications could be lost!
+*/
+
+--define=ICALL_RAM0_START=0x20004318
+--define=ICALL_STACK0_START=0xc000
+--define=ICALL_STACK0_ADDR=0xc001
+
